7a3c180dae5687875992c654e67d839203a98382
Gladys <gladysalu1@gmail.com>
2024-03-14 13:54:26 +0100

    commit

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -20,10 +20,10 @@ const UserList = () => {
     },[])
 
     return (
-        <div className="flex justify-center ">
-            <div> 
+        <div>
             <h1 className="text-center text-3xl font-bold mb-5 mt-4">USER LIST</h1>
-                {userList.map(user => (<div key={user.id} className="flex flex-col bg-lime-200 mb-6 ml-3 p-5 justify-center   rounded-2xl gap-2 tracking-wide shadow-md"><h1><b>NAME:</b> <span className="text-lg">{user.name}</span></h1>
+            <div className="flex flex-wrap justify-center items-center">
+                {userList.map(user => (<div key={user.id} className="flex flex-col bg-lime-200 mb-6 ml-3 p-5 justify-center   rounded-2xl gap-2 tracking-wide shadow-md m-3 w-[23rem]"><h1><b>NAME:</b> <span className="text-lg">{user.name}</span></h1>
                 <h3><b>USERNAME:</b>{user.username}</h3>
                 <h3><b>EMAIL:</b> {user.email}</h3>
                 <h3><b>WEBSITE:</b> {user.website}</h3>
